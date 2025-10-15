e24f33839b0ba09f2d73578897486c9eafa8a8c4
Jarne Adam <jarne.adam@student.vives.be>
2025-10-15 12:24:03 +0200

    Added styles for the list and the title

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { issues } from '../../issues';
 import Title from './Title';
 
@@ -14,16 +14,44 @@ const List = () => {
         issues
           .filter(issue => issue.status == 'Open')
           .map(issue => (
-            <View>
-              <Text>Id: {extractId(issue.id)}</Text>
-              <Text>Omschrijving: {issue.description}</Text>
-              <Text>Toegewezen aan: {toUpper(issue.assigned)}</Text>
-              <Text>----------</Text>
+            <View key={issue.id} style={[styles.issueItem, issue.assigned && issue.assigned.toLowerCase() === 'katerina larson' ? styles.katerina : styles.other]}>
+              <Text>
+                <Text style={styles.propName}>Id: </Text>
+                {extractId(issue.id)}
+              </Text>
+              <Text>
+                <Text style={styles.propName}>Omschrijving: </Text>
+                {issue.description}
+              </Text>
+              <Text>
+                <Text style={styles.propName}>Toegewezen aan: </Text>
+                {toUpper(issue.assigned)}
+              </Text>
             </View>
           ))
       }
     </View>
-  );  
+  );
 }
+
+const styles = StyleSheet.create({
+  issueItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    borderStyle: 'solid',
+    marginBottom: 5,
+    paddingVertical: 4,
+    paddingHorizontal: 6,
+  },
+  propName: {
+    fontWeight: '600'
+  },
+  katerina: {
+    backgroundColor: 'rgba(243,183,183,0.39)'
+  },
+  other: {
+    backgroundColor: 'rgba(147,147,210,0.55)'
+  }
+});
 
 export default List;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
-const Title = () => <Text>Open issues</Text>;
+const Title = () => <Text style={styles.container}>Open issues</Text>;
+
+const styles = StyleSheet.create({
+    container: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingBottom: 5
+    },
+});
 
 export default Title;
